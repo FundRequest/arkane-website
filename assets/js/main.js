22bfe77ba4a8d79dc46791553dda9fb2771a4f53
Steve Maris <steve_maris@hotmail.com>
2018-09-06 20:45:50 +0200

    Add mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,58 @@
-document.getElementById('arkane-sign').addEventListener('click', function() {
-  ArkaneConnect.signTransaction({
-    type: 'ETHEREUM_TRANSACTION',
-    walletId: 44,
-    submit: false,
-    gasPrice: 10000000000,
-    gas: 23000,
-    nonce: 0,
-    value: 10000000000,
-    data: '0x',
-    to: '0xdc71b72db51e227e65a45004ab2798d31e8934c9'
-  }).then(function(result) {
-    console.log(result);
-  }).catch(function(error) {
-    console.log(error);
+(function(w, $) {
+  'use strict';
+
+  function replacePageBg() {
+    $('[data-page-bg]').each(function() {
+      var $bg = $(this);
+      var src = $bg.attr('data-page-bg');
+      var $bgBig = $('[data-page-bg-big]');
+
+      $('<img>').attr('src', src).on('load', function() {
+        $bgBig.css('background-image', 'url("' + src + '")');
+        $bg.addClass('fadeOut');
+        $bgBig.addClass('fadeIn');
+      });
+
+    });
+  }
+
+  function initToggle() {
+    var event = 'ontouchstart' in document.documentElement ? 'touchstart' : click;
+
+    $('[data-toggle]').on(event, function() {
+      var $toggleButton = $(this);
+      var $body = $('body');
+      var isOpen = $toggleButton.hasClass('open');
+      var $toggleItem = $($toggleButton.data('toggle'));
+      console.log($toggleItem);
+      if(isOpen) {
+        $body.css('overflow', 'auto');
+        $toggleButton.removeClass('open');
+        if($toggleItem.length > 0) {
+          $toggleItem.removeClass('open');
+        }
+      } else {
+        $body.css('overflow', 'hidden');
+        $toggleButton.addClass('open');
+        if($toggleItem.length > 0) {
+          $toggleItem.addClass('open');
+        }
+      }
+    });
+  }
+
+  function openMenu(){
+    $('div.circle').addClass('expand');
+    $('.menu li').addClass('animate');
+  }
+
+  function closeMenu(){
+    $('div.circle').removeClass('expand');
+    $('.menu li').removeClass('animate');
+  }
+
+  $(function() {
+    replacePageBg();
+    initToggle();
   });
-});
+})(window, jQuery);
